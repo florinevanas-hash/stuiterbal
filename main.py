4a65225d0148bcc4ef66bae8238b593be83d8659
florinevanas-hash <florinevanas@gmail.com>
2026-05-20 09:57:04 +0200

    begin space rat 1

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 import pygame
-from sys import exit
+import math
+import random
 
 pygame.init()
-screen = pygame.display.set_mode((800,400))
+
+W, H = 480, 640
+screen = pygame.display.set_mode((W, H))
 pygame.display.set_caption("Space Rat")
 clock = pygame.time.Clock()
 
-test_surface = pygame.Surface((100, 200))
-test_surface.fill('red')
+GRAVITY     = 0.45
+FLAP        = -8.5
+PIPE_SPEED  = 2.8
+PIPE_W      = 60
+PIPE_GAP    = 160
+RAT_X       = 90
+RAT_R       = 20
+PIPE_FRAMES = 90
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-    
-    screen.blit(test_surface, (0, 0))
-    pygame.display.update()
-    clock.tick(60)
+try:
+    font_lg = pygame.font.SysFont("Courier New", 42, bold=True)
+    font_md = pygame.font.SysFont("Courier New", 22)
+    font_sm = pygame.font.SysFont("Courier New", 15)
+except Exception:
+    font_lg = pygame.font.SysFont(None, 42)
+    font_md = pygame.font.SysFont(None, 22)
+    font_sm = pygame.font.SysFont(None, 15)
+
+
   
 
 
