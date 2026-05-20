f174c8b7f5b1e6a7baed0078dce7f7d38a828ce6
florinevanas-hash <florinevanas@gmail.com>
2026-05-20 11:56:33 +0200

    rainbow and more changes

--- main.py
+++ main.py
@@ -13,17 +13,30 @@ GRAVITY     = 0.45
 FLAP        = -8.5
 PIPE_SPEED  = 2.8
 PIPE_W      = 60
-PIPE_GAP    = 160
+PIPE_GAP    = 200
 RAT_X       = 90
 RAT_R       = 20
 PIPE_FRAMES = 90
+TRAIL_LEN     = 18
+
+RAINBOW = [
+    (255, 0,   0),
+    (255, 127, 0),
+    (255, 220, 0),
+    (0,   220, 0),
+    (0,   100, 255),
+    (80,  0,   200),
+    (200, 0,   200),
+]
 
 try:
-    font_lg = pygame.font.SysFont("Courier New", 42, bold=True)
-    font_md = pygame.font.SysFont("Courier New", 22)
+    font_xl = pygame.font.SysFont("Courier New", 46, bold=True)
+    font_lg = pygame.font.SysFont("Courier New", 38, bold=True)
+    font_md = pygame.font.SysFont("Courier New", 22, bold=True)
     font_sm = pygame.font.SysFont("Courier New", 15)
 except Exception:
-    font_lg = pygame.font.SysFont(None, 42)
+    font_xl = pygame.font.SysFont(None, 46)
+    font_lg = pygame.font.SysFont(None, 38)
     font_md = pygame.font.SysFont(None, 22)
     font_sm = pygame.font.SysFont(None, 15)
 
